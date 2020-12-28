92773c1f6cf019b5c171525ed087c8709563a205
specialsalsa <48301284+specialsalsa@users.noreply.github.com>
2020-12-27 23:23:14 -0800

    Update clearinterval.js
    added permission check

--- clearinterval.js
+++ clearinterval.js
@@ -2,7 +2,14 @@ module.exports = {
     name: 'stopmessage',
     description: 'clears recurring message',
     execute(message, args) {
+        if (message.member.roles.cache.find(r => r.name === "Admin") 
+        || message.member.roles.cache.find(r => r.name === "Mod") 
+        || message.member.roles.cache.find(r => r.name === "Developer")
+         ) {
             message.channel.send('Stopping recurring message.');
             clearInterval(interval);
+        } else {
+            message.reply("you don't have permission to do that!");
+        }
     }
 };
