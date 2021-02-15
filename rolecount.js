0218a308a4f431eb25a6f85fb6db6117bbadd478
specialsalsa <48301284+specialsalsa@users.noreply.github.com>
2021-02-15 15:14:23 -0800

    Add files via upload

--- rolecount.js
+++ rolecount.js
@@ -2,14 +2,12 @@ module.exports = {
     name: 'rolecount',
     description: 'counts number of users with a certain role',
     execute(message, args) {
-        if (args[0]) {
-            try {
-                const Role = message.guild.roles.cache.find(role => role.name == args[0]);
-                const Members = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => member.user.tag);
-                message.channel.send(`Number of users with ${Role.name}: ${Members.length}`);
-            } catch (e) {
-                message.channel.send(`Please enter a valid role (roles are case-sensitive).`);
-            }
+        try {
+            const Role = message.guild.roles.cache.find(role => role.name == args[0]);
+            const Members = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => member.user.tag);
+            message.channel.send(`Number of users with ${Role.name}: ${Members.length}`);
+        } catch (e) {
+            message.channel.send(`Please enter a valid role (roles are case-sensitive).`);
         }
     }
 }
