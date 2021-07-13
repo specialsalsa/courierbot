383684ef620e30076d864d029c93d39b57af8f47
specialsalsa <48301284+specialsalsa@users.noreply.github.com>
2021-07-13 13:37:13 -0700

    fixed up express server

--- courierbot.js
+++ courierbot.js
@@ -14,15 +14,6 @@ con.getConnection(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
-
-//initializing express server
-const PORT = 3000;
-const INDEX = "/index.html";
-
-const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 // initial prefix setting
 let prefix = ".";
 
