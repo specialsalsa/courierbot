7b2c57f8c1e6e2b6131c5928e5c03e0c145793d0
specialsalsa <48301284+specialsalsa@users.noreply.github.com>
2021-10-22 21:38:10 -0700

    .rolecount uses destructuring now

--- commands/rolecount.js
+++ commands/rolecount.js
@@ -3,32 +3,26 @@ module.exports = {
   description: 'counts number of users with a certain role',
   async execute(message, args) {
     let role = args.join(' ');
-    let roleID;
-    let roleOriginalCase;
+    let roleID, roleOriginalCase;
+
     try {
-      roleID = message.guild.roles.cache.find(
+      ({ id: roleID, name: roleOriginalCase } = message.guild.roles.cache.find(
         r => r.name.toLowerCase() === role.toLowerCase()
-      ).id;
-      roleOriginalCase = message.guild.roles.cache.find(
-        r => r.name.toLowerCase() === role.toLowerCase()
-      ).name;
+      ));
     } catch (err) {
-      console.log(`.rolecount executed with unknown role ${role}`);
+      message.channel.send(`role "${role}" not found`);
+      return;
     }
 
-    if (roleID) {
-      let guildMembers = await message.guild.members.fetch();
-      let memberCounter = 0;
-      guildMembers.forEach(member => {
-        if (member._roles.includes(roleID)) {
-          memberCounter++;
-        }
-      });
-      message.channel.send(
-        `Number of members with role "${roleOriginalCase}": ${memberCounter}`
-      );
-    } else {
-      message.channel.send(`role "${roleOriginalCase}" not found`);
-    }
+    let guildMembers = await message.guild.members.fetch();
+    let memberCounter = 0;
+    guildMembers.forEach(member => {
+      if (member._roles.includes(roleID)) {
+        memberCounter++;
+      }
+    });
+    message.channel.send(
+      `Number of members with role "${roleOriginalCase}": ${memberCounter}`
+    );
   }
 };
