df3133e47c8a9e2a9cbef570e72b43a6d457c7d7
specialsalsa <48301284+specialsalsa@users.noreply.github.com>
2021-10-22 20:31:11 -0700

    .rolecount returns original case of role

--- commands/rolecount.js
+++ commands/rolecount.js
@@ -2,19 +2,33 @@ module.exports = {
   name: 'rolecount',
   description: 'counts number of users with a certain role',
   async execute(message, args) {
-    let role = args.join(' ').toLowerCase();
-    let roleID = message.guild.roles.cache.find(
-      r => r.name.toLowerCase() === role
-    ).id;
-    let guildMembers = await message.guild.members.fetch();
-    let memberCounter = 0;
-    guildMembers.forEach(member => {
-      if (member._roles.includes(roleID)) {
-        memberCounter++;
-      }
-    });
-    message.channel.send(
-      `Number of members with role "${role}": ${memberCounter}`
-    );
+    let role = args.join(' ');
+    let roleID;
+    let roleOriginalCase;
+    try {
+      roleID = message.guild.roles.cache.find(
+        r => r.name.toLowerCase() === role.toLowerCase()
+      ).id;
+      roleOriginalCase = message.guild.roles.cache.find(
+        r => r.name.toLowerCase() === role.toLowerCase()
+      ).name;
+    } catch (err) {
+      console.log(`.rolecount executed with unknown role ${role}`);
+    }
+
+    if (roleID) {
+      let guildMembers = await message.guild.members.fetch();
+      let memberCounter = 0;
+      guildMembers.forEach(member => {
+        if (member._roles.includes(roleID)) {
+          memberCounter++;
+        }
+      });
+      message.channel.send(
+        `Number of members with role "${roleOriginalCase}": ${memberCounter}`
+      );
+    } else {
+      message.channel.send(`role "${roleOriginalCase}" not found`);
+    }
   }
 };
